2c2746e36602a9a3c0072f9eaeb8af9297fda038
Jerrod <JerrodP@live.com>
2023-06-25 20:20:06 -0500

    #1 Gather stats on ISBN 10 or 13

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from scraper import Book
 
 
 def main():
-    print("Enter the ISBN you'd like to search: ")
-    isbn = input()
+    isbn = "978-0143039433"
+    # isbn = "0143039431"
     isbn = Book.parse_isbn(isbn)
     book_stats = Book.fetch_amazon_stats(isbn)
     print(book_stats)
