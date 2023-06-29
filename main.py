06369be363a5573079584968782d49f59269d15b
Jerrod <JerrodP@live.com>
2023-06-28 21:56:17 -0500

    Fix requirements.txt and update DB destructor

--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 from src.scraper import Scraper
 from src.database import Database
 
+
 def main():
-    isbn = "978-1400079278"
+    isbn = "978-1635575552"
     # isbn = "0143039431"
     isbn = Scraper.parse_isbn(isbn)
     book_stats = Scraper.fetch_amazon_stats(isbn)
     print(book_stats)
     DB = Database()
     DB.add_book(book_stats)
-
 
 
 if __name__ == "__main__":
